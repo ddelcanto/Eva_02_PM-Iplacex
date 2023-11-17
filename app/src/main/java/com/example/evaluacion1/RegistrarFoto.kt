ee4d346c28a86855c871bfc01d6d7deb89bfe8f7
Daniel del Canto Arce <daniel.delcanto.arce@outlook.com>
2023-11-17 16:35:11 -0300

    1635

--- app/src/main/java/com/example/evaluacion1/RegistrarFoto.kt
+++ app/src/main/java/com/example/evaluacion1/RegistrarFoto.kt
@@ -1,14 +1,11 @@
 package com.example.evaluacion1
 
 import android.app.Activity
-import android.content.ContentValues
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
